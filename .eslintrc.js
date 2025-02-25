95ee777e336d5730b546e0d84858717613dde9bb
Kevin Bravo <bravokevinto@gmail.com>
2025-02-24 21:29:58 -0400

    feat: dissable eslint rule

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-// .eslintrc.js (or .eslintrc.json, package.json)
-module.exports = {
-  // ... other configurations
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"]
-  },
-};
